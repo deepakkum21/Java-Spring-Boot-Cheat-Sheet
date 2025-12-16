e6f64ccb9fe1b3531b5b393601928e54e163bb38
Deepk kumar <deepakanjani21@gmail.com>
2025-12-16 22:22:25 +0530

    structure change

---
+++ rest-feign/rest-client/src/main/java/com/rest/client/rest_client/RestClientApplication.java
@@ -0,0 +1,13 @@
+package com.rest.client.rest_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientApplication.class, args);
+	}
+
+}
